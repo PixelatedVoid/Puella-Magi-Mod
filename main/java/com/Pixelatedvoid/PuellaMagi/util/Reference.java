24874f2d8893d707b08a34483ee808a67443df5c
PixelatedVoid <53279350+PixelatedVoid@users.noreply.github.com>
2019-07-24 18:10:00 -0400

    Add files via upload

---
+++ main/java/com/Pixelatedvoid/PuellaMagi/util/Reference.java
@@ -0,0 +1,12 @@
+package com.Pixelatedvoid.PuellaMagi.util;
+
+public class Reference {
+
+		public static final String MOD_ID = "pmm";
+		public static final String NAME = "Puella Magi";
+		public static final String VERSION = "1.0";
+		public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+		public static final String CLIENT_PROXY_CLASS = "com.Pixelatedvoid.PuellaMagi.proxy.ClientProxy";
+		public static final String COMMON_PROXY_CLASS = "com.Pixelatedvoid.PuellaMagi.proxy.CommonProxy";
+//All this is doing is setting up commonly used string values. If it's referenced here it's easier to use in other classes.
+}
